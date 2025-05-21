f324ed0dad60ab198639e8c38186fbe30315e812
Brian-09 <bcarver157@gmail.com>
2025-05-20 21:20:50 -0700

    small thing added to a js file
    not finished

--- scripts/linkHover.js
+++ scripts/linkHover.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-    let navLink = $("#nav-link")
+    let navLink = $(".nav-link")
 
-    
+    navLink.mouseover(function(){
+        navLink.animate({top: "100px"}, 1000)
+    })
+    navLink.mouseout(function(){
+        // code
+    })
 });
